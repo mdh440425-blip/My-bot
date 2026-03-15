7e6e085c4ea464f2046a25e1fb4386b5a93621a3
mdh440425-blip <mdh440425@gmail.com>
2026-03-15 21:39:37 +0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const port = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.end("Bot is running!");
+}).listen(port, () => {
+  console.log("Server running on port " + port);
+});
